4d4b09f8c2af750b9d5b3b06a8c7c01c3a94683b
robert kindinger <rkindinger@outlook.com>
2016-09-01 21:03:47 -0400

    Project 2 incomplete

---
+++ .idea/Randint.intro.py
@@ -0,0 +1,39 @@
+
+#import random
+#randomNumber = random.randint(0,10)
+#print "the number is: " + str(randomNumber)
+
+#robert kindinger.
+#project 2.
+#Game name whos Number is it anyway
+
+import random
+
+print "The Number game, where the numbers are random and the points dont matter"
+
+game_mode = raw_input("choose difficulty level. easy, medium, hard")
+if game_mode == "easy":
+    easy = int(raw_input("pick number between 0 , 3")
+    random_number = random.randint(0, 3)
+    if easy == random_number
+    print + "is correct."
+elif game_mode == "medium":
+    # ask the user to pick a number
+    # generate random number
+    # check if they are correct or not
+
+
+
+
+print "Mode: Easy "
+randomNumber = random.radint(0,3)
+easy = int(raw_input("Pick a number between 0 and 3"))
+if randomNumber == guess_one
+    print Guess_one + " you are correct. 100 points are rewarded"
+elif randomNumber != guess_one
+    print guess_one + " you are incorrect. 200 points are reducted"
+
+
+print "Mode: Medium "
+randomNumber = random.radint(6,15)
+guess_two = int(raw_input
---
+++ operators.Intro.py
@@ -0,0 +1,18 @@
+# Operators.Intro
+# Robert kindinger
+# NO qlue how im going to pull this out of my ass, but lets try anyways
+
+print "Addition"
+add_one = int(raw_input("enter number"))
+add_two = int(raw_input("enter number"))
+print "the sum of these values are ", add_one + add_two
+
+print "Subtraction"
+sub_one = int(raw_input("enter number"))
+sub_two = int(raw_input("enter number"))
+print "the answer is  ", sub_one - sub_two
+
+print "multiplication"
+mult_one = int(raw_input("enter number"))
+mult_two = int(raw_input("enter number"))
+print "the product of these sums is ", mult_one * mult_two
